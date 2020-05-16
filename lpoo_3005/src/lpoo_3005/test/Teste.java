8292ce52a26758581956e955aecc56d9696e82b8
tokyozinn <andre.yabiku@hotmail.com>
2020-05-16 17:29:31 -0300

    Criada primeira bateria de testes, referente a saques.

--- lpoo_3005/src/lpoo_3005/test/Teste.java
+++ lpoo_3005/src/lpoo_3005/test/Teste.java
@@ -1,37 +1,57 @@
 package lpoo_3005.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 import lpoo_3005.banco.Banco;
 import lpoo_3005.banco.TipoContas;
 
 public class Teste {
 
-	
-	public static void main(String[] args) throws Exception {
+	@Test
+	public void saqueContaMinimaTest() throws Exception {
+		
 		Banco b = new Banco();
 		
-		b.cadastraCliente("14", "Ana");
-		b.cadastraCliente("13", "Ana Banana");
-		b.cadastraCliente("22", "Bruna");
-		b.cadastraCliente("21", "Bia");
-		b.cadastraCliente("30", "Cassia");
-		b.cadastraCliente("39", "Carol");
-		b.cadastraCliente("48", "Drauzio");
-		b.cadastraCliente("74", "Duda");
-		b.cadastraCliente("65", "Fernanda");
-		b.cadastraCliente("55", "Flavia");
-		b.cadastraCliente("64", "Gabi");
-		b.cadastraCliente("36", "Gio");
-		b.cadastraCliente("72", "Jack");
-		b.cadastraCliente("17", "Joana");
-
+		b.cadastraCliente("100", "Fulano");
+		b.setEndereco("100", "Rua X", 140);
+		b.setConta("100", TipoContas.MINIMA);
 		
-		b.setEndereco("14", "Rua jafágá", 14);
-
+		b.depositar("100", 1000.0);
+		b.sacar("100", 500.0);
 		
-		b.setConta("14", TipoContas.STANDARD);
-		b.setChequeEspecial("14", 5000);
-		b.depositar("14", 2000);
-		b.sacar("14", 6000);
-		
-		System.out.println(b.getClientes());
+		assertEquals(500.0, b.buscaCliente("100").getConta().getSaldo(), 0.00001);
 	}
-}
+	
+	@Test
+	public void saqueContaStandardComChequeEspecial() throws Exception {
+
+		Banco b = new Banco();
+		
+		b.cadastraCliente("200", "Ciclano");
+		b.setEndereco("200", "Rua Y", 240);
+		b.setConta("200", TipoContas.STANDARD);
+		b.setChequeEspecial("200", 5000);
+		
+		b.sacar("200", 4000.0);
+		
+		assertEquals(-4000.0, b.buscaCliente("200").getConta().getSaldo(), 0.00001);
+	}
+	
+	@Test
+	public void saqueContaUmPorcentoComSaldoEChequeEspecial() throws Exception {
+		Banco b = new Banco();
+	
+		b.cadastraCliente("300", "Beltrano");
+		b.setEndereco("300", "Rua Z", 340);
+		b.setConta("300", TipoContas.UM_PORCENTO);
+		
+		b.setChequeEspecial("300", 8000);
+		b.depositar("300", 2000.0);
+		b.sacar("300", 10000.00);
+		
+		assertEquals(-8000.0, b.buscaCliente("300").getConta().getSaldo(), 0.0001);
+	}
+
+	}
+
