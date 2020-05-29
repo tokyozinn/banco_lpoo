af7df54378aa6adaa659a7a77dbc0109c5565cd8
tokyozinn <andre.yabiku@hotmail.com>
2020-05-29 11:28:49 -0300

    Corrigindo classe enum 'TipoContas'

--- lpoo_3005/src/lpoo_3005/banco/TipoContas.java
+++ lpoo_3005/src/lpoo_3005/banco/TipoContas.java
@@ -2,10 +2,6 @@ package lpoo_3005.banco;
 
 public enum TipoContas {
 
-//	MINIMA(new ContaMinima((int)(Math.random()*10000), 0.0)),
-//	STANDARD(new ContaStandard((int)(Math.random()*10000), 10.0)),
-//	UM_PORCENTO(new ContaUmPorcento((int)(Math.random()*10000), 20.0));
-
 	MINIMA, STANDARD, UM_PORCENTO;
 	
 }
